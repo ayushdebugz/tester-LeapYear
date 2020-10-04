a42385c6de3ec089cc0cf0a3a0ba7bbb5b170b02
AYUSH JHA <67900051+ayushdebugz@users.noreply.github.com>
2020-10-04 19:00:34 +0530

    Add files via upload

---
+++ LeapYeaRTester.py
@@ -0,0 +1,17 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 12 20:34:41 2018
+
+@author: Ayush Jha;
+"""
+# Spyder Program "Leap Year" Or "Not"
+# Leap -Not Leap Program using '%' Operator
+
+x=int(input(" Please Enter N E Year:"));
+if(x%4==0):
+    print("Given Year Is Leap One!")
+else:
+    print("Given Year Is Not A Leap One!")
+    
+
